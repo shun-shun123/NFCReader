96f72f72590054b1ccdc08744f4e8528f328e2eb
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-29 17:32:51 +0900

    list.clear()を追加して重ねることを止める

--- app/src/main/java/suika/jp/nfcreader/Json/Suika.kt
+++ app/src/main/java/suika/jp/nfcreader/Json/Suika.kt
@@ -1,5 +1,5 @@
 package suika.jp.nfcreader.Json
 
-data class Suika(val terminal: String?, val process: String?, val entranceDistrict: String?, val entranceRoute: String?, val entranceStation: String?, val exitDistrict: String?, val exitRoute: String?, val exitStation: String?, val dataProcess: String?, val balance: Int?) {
+data class Suika(val terminal: String?, val process: String?, val entranceDistrict: String?, val entranceRoute: String?, val entranceStation: String?, val exitDistrict: String?, val exitRoute: String?, val exitStation: String?, val date: String?, val balance: Int?) {
 
 }
